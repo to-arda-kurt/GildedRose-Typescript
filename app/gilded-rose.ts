4f05508d8ffeefbf3948536031590bf7caadeab3
Arda Kurt <to.arda.kurt@gmail.com>
2023-07-14 10:03:44 +0100

    Gilded Rose V1 Done

--- app/gilded-rose.ts
+++ app/gilded-rose.ts
@@ -17,7 +17,7 @@ export class GildedRose {
         this.items = items;
     }
 
-    changeDay(day: number):number{
+    changeDay(day: number): number {
         return day - 1;
     }
 
@@ -26,28 +26,28 @@ export class GildedRose {
         return sellIn;
     }
 
-    agedBrie(item: Item):Item{
+    agedBrie(item: Item): Item {
 
-        item.quality = item.sellIn<0 ? item.quality + 2 : item.quality + 1
+        item.quality = item.sellIn < 0 ? item.quality + 2 : item.quality + 1
         item.quality = this.evenFifty(item.quality);
         return item;
     }
 
-    backStage(item: Item):Item{
+    backStage(item: Item): Item {
 
-        if(item.sellIn > 10 && item.sellIn <50) {
+        if (item.sellIn >= 10 ) {
             item.quality = item.quality + 1
         }
 
-        if(item.sellIn < 10 && item.sellIn >= 5) {
+        if (item.sellIn >= 5) {
             item.quality = item.quality + 2
         }
 
-        if(item.sellIn < 5 && item.sellIn >= 0) {
+        if (item.sellIn >= 0) {
             item.quality = item.quality + 3
         }
-        
-        if(item.sellIn < 0){
+
+        if (item.sellIn < 0) {
             item.quality = 0;
         }
 
@@ -55,47 +55,48 @@ export class GildedRose {
         return item;
     }
 
-    sulfuras(item:Item):Item {
+    sulfuras(item: Item): Item {
         item.quality = 80;
         item.sellIn = 0;
         return item;
     }
 
-    basicItems(item:Item):Item{
-        item.quality = item.sellIn < 0 ? item.quality - 2 : item.quality - 1;  
+    basicItems(item: Item): Item {
+        item.quality = item.sellIn < 0 ? item.quality - 2 : item.quality - 1;
         return item;
     }
 
-    isConjured(name:string):boolean{
+    isConjured(name: string): boolean {
         return name.startsWith('Conjured' || 'conjured');
     }
 
-    conjured(item:Item):Item {
-        item.quality = item.sellIn < 0 ? item.quality - 4 : item.quality-2;
+    conjured(item: Item): Item {
+        item.quality = item.sellIn < 0 ? item.quality - 4 : item.quality - 2;
         return item;
 
     }
 
     updateQuality() {
         // for (let i = 0; i < this.items.length; i++)
-        this.items = this.items.map(item=> {
+        this.items = this.items.map(item => {
 
-            item.sellIn  = this.changeDay(item.sellIn);
-            const isConjured = this.isConjured(item.name)
+            item.sellIn = this.changeDay(item.sellIn);
 
-            if(isConjured) return this.conjured(item);
+            const isConjured = this.isConjured(item.name) 
+
+            if (isConjured) return this.conjured(item);
 
             switch (item.name) {
-                
+
                 case 'Aged Brie':
                     return this.agedBrie(item);
-    
+
                 case 'Backstage passes to a TAFKAL80ETC concert':
                     return this.backStage(item);
-                
+
                 case 'Sulfuras, Hand of Ragnaros':
                     return this.sulfuras(item);
-                                  
+
                 default:
                     return this.basicItems(item);
 
@@ -103,9 +104,7 @@ export class GildedRose {
         })
 
         return this.items;
-            
-        
-       
+
     }
-   
+
 }
